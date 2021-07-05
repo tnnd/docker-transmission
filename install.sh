873416dd18dbb94db06dad561b0dd03211603404
tnnd <37441897+tnnd@users.noreply.github.com>
2021-07-05 18:21:53 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ apk update
 #进入root目录
 cd /root
 #安装所需软件
-apk add wget transmission-daemon
+apk add curl transmission-daemon
 
 #安装timezone
 apk add -U tzdata
@@ -15,15 +15,6 @@ ls /usr/share/zoneinfo
 cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 #查看当前时间
 date
-#为了精简镜像，可以将tzdata删除了
-apk del tzdata
-apk del wget
-
-#安装transmission美化
-cd /root
-wget https://github.com/ronggang/transmission-web-control/raw/master/release/install-tr-control-cn.sh
-chmod +x install-tr-control-cn.sh
-sh install-tr-control-cn.sh
 
 #初始化任务
 /usr/bin/transmission-daemon
@@ -32,10 +23,13 @@ sleep 10s
 #杀死任务
 pkill transmission-daemon
 #修改配置文件
-sed -i "s%\"rpc-whitelist\": \"127.0.0.1,::1\"%\"rpc-whitelist\": \"0.0.0.0\"%g" /root/.config/transmission-daemon/settings.json
-sed -i "s%\"rpc-whitelist-enabled\": true%\"rpc-whitelist-enabled\": false%g" /root/.config/transmission-daemon/settings.json
-sed -i "s%\"rpc-authentication-required\": false%\"rpc-authentication-required\": true%g" /root/.config/transmission-daemon/settings.json
+cd /root/.config/transmission-daemon
+curl -OL https://raw.githubusercontent.com/tnnd/sh/master/transmission/settings.json
 sed -i "s%\"cache-size-mb\": 4%\"cache-size-mb\": 64%g" /root/.config/transmission-daemon/settings.json
+
+#为了精简镜像，可以将tzdata删除了
+apk del tzdata
+apk del curl
 #重命名配置文件目录，启动备用
 mv /root/.config/transmission-daemon /root/.config/transmission-daemon.bak
 mkdir -p /root/.config/transmission-daemon

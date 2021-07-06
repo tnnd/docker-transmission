522274be929ca769c14f9b08e9bbffa953266caf
tnnd <37441897+tnnd@users.noreply.github.com>
2021-07-06 10:24:55 +0800

    Create install.sh

--- install.sh
+++ install.sh
@@ -7,15 +7,6 @@ cd /root
 #安装所需软件
 apk add curl transmission-daemon
 
-#安装timezone
-apk add -U tzdata
-#查看时区列表
-ls /usr/share/zoneinfo
-#拷贝需要的时区文件到localtime
-cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-#查看当前时间
-date
-
 #初始化任务
 /usr/bin/transmission-daemon
 #延长20s
@@ -26,10 +17,8 @@ pkill transmission-daemon
 cd /root/.config/transmission-daemon
 curl -OL https://raw.githubusercontent.com/tnnd/sh/master/transmission/settings.json
 sed -i "s%\"cache-size-mb\": 4%\"cache-size-mb\": 64%g" /root/.config/transmission-daemon/settings.json
-
-#为了精简镜像，可以将tzdata删除了
-apk del tzdata
 apk del curl
+
 #重命名配置文件目录，启动备用
 mv /root/.config/transmission-daemon /root/.config/transmission-daemon.bak
 mkdir -p /root/.config/transmission-daemon
